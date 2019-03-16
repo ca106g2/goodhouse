f9214010c87afa29ed26426e371fbf2bd3be1366
Rick <Rick@LAPTOP-JVLTJHOA>
2019-03-16 17:49:31 +0800

    新增積分商城圖片+資料庫新增圖片方法

--- WebContent/資料庫/set_House_pic_from_file.sql
+++ WebContent/資料庫/set_House_pic_from_file.sql
@@ -17,3 +17,20 @@ BEGIN
     dbms_lob.fileclose(myBFILE);
     RETURN myBLOB;
 END load_blob;
+
+/* 分開執行*/
+/* 積分商城圖片 */ 
+CREATE OR REPLACE  DIRECTORY PG_DIR AS 'C:/pointgoods_pic/'; 
+
+/* 積分商城圖片擷取檔案的FUNCTION */ 
+CREATE OR REPLACE FUNCTION load_pg( myFileName VARCHAR) RETURN BLOB as result BLOB;  
+  myBFILE      BFILE;
+  myBLOB       BLOB;
+BEGIN
+    myBFILE := BFILENAME('PG_DIR',myFileName);
+    dbms_lob.createtemporary(myBLOB, TRUE);
+    dbms_lob.fileopen(myBFILE,dbms_lob.file_readonly);
+    dbms_lob.loadfromfile(myBLOB,myBFILE,dbms_lob.getlength(myBFILE) );
+    dbms_lob.fileclose(myBFILE);
+    RETURN myBLOB;
+END load_pg;
